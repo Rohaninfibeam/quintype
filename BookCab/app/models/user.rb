22450bbe832ef86fb54a378e36e809b09e13660d
rohan <rohanbr148@gmail.com>
2017-09-14 14:37:54 +0530

    devise and cancan gem install

--- BookCab/app/models/user.rb
+++ BookCab/app/models/user.rb
@@ -1,2 +1,26 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  before_save :assign_role
+
+  def admin?
+  	role=="admin"
+  end
+
+  def driver?
+  	role=="driver"
+  end
+
+  def customer?
+  	role=="customer"
+  end
+
+  private
+
+  def assign_role
+  	debugger
+  	self.role="user"
+  end
 end
