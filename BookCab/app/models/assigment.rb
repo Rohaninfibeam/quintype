c78e3e859de2acd2146bcd9a1e2210fed60f4453
rohan <rohanbr148@gmail.com>
2017-09-18 01:33:50 +0530

    assigment of cab to user

--- BookCab/app/models/assigment.rb
+++ BookCab/app/models/assigment.rb
@@ -1,2 +1,66 @@
 class Assigment < ApplicationRecord
+	belongs_to :user
+	belongs_to :cab
+
+	def set_source_location(latitude, longitude)
+		latitude = latitude.to_f
+		longitude = longitude.to_f
+		self.source_lat = latitude
+		self.source_long = longitude
+		self.save!
+	end
+
+	def set_dest_location(latitude,longitude)
+		latitude = latitude.to_f
+		longitude = longitude.to_f
+		self.dest_lat = latitude
+		self.dest_long = longitude
+		self.save!
+	end
+
+	def get_source_location
+		[self.source_lat,self.source_long]
+	end
+
+	def get_dest_location
+		[self.dest_lat,self.dest_long]
+	end
+
+	def start_ride(latitude,longitude)
+		set_source_location(latitude,longitude)
+		self.start_time = Time.now
+		self.save!
+	end
+
+	def end_ride(latitude,longitude)
+		set_dest_location(latitude,longitude)
+		self.end_time = Time.now
+		self.distance = find_distance.present? ? find_distance : nil
+		self.price = find_total_price.present? ? find_total_price : nil
+		self.save!
+	end
+
+	def cancel_ride
+		self.cancelled_time = Time.now
+		self.save!
+	end
+
+	private
+
+	def find_distance
+		if self.end_time.present?
+			total_distance(get_dest_location,get_source_location)
+		else
+			nil
+		end
+	end
+
+	def find_total_price
+		if self.distance.present? && self.start_time.present? && self.end_time.present?
+			total_time = (self.end_time- self.start_time)/60
+			total_time*1 + distance*2
+		else
+			nil
+		end
+	end
 end
--- BookCab/test/models/assigment_test.rb
+++ BookCab/test/models/assigment_test.rb
@@ -4,4 +4,45 @@ class AssigmentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_start_test
+  	cab = cabs(:one)
+  	user = users(:one)
+  	cab = cab.cab_assign(user)
+  	cab.start_ride(11.23, 34.45)
+  	cab.reload
+  	assigment = cab.assigments.last
+  	assert assigment.start_time != nil && assigment.source_lat == 11.23 && assigment.source_long == 34.45
+  end
+
+  def test_get_lat_long_source
+  	cab = cabs(:one)
+  	user = users(:one)
+  	cab = cab.cab_assign(user)
+  	assigment = cab.assigments.last
+  	assigment.set_source_location(11.23, 34.45)
+  	assigment.reload
+  	assert assigment.get_source_location == [11.23 , 34.45]
+  end
+
+  def test_get_lat_long_destination
+  	cab = cabs(:one)
+  	user = users(:one)
+  	cab = cab.cab_assign(user)
+  	assigment = cab.assigments.last
+  	assigment.set_dest_location(11.22, 14.45)
+  	assigment.reload
+  	assert assigment.get_dest_location == [11.22, 14.45]
+  end
+
+  def test_end_ride
+  	cab = cabs(:one)
+  	user = users(:one)
+  	cab = cab.cab_assign(user)
+  	cab.start_ride(11.23, 34.45)
+  	cab.end_ride(11.53, 14.45)
+  	debugger
+  	assigment = cab.assigments.last
+  	assert assigment.end_time != nil && assigment.dest_lat == 11.53 && assigment.dest_long == 14.45 && assigment.distance && assigment.price
+  end
+
 end
